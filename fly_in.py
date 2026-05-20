1ffc7331188bf084d9e4030ba100d65b0bbee4da
stmaire <steffi.maire@gmail.com>
2026-05-20 17:48:58 +0200

    structure parsing

---
+++ fly_in.py
@@ -0,0 +1,38 @@
+import sys
+from pydantic import ValidationError
+from loader import Loader
+from parser import MapParser
+from models import MapConfigModel
+
+
+def main() -> None:
+    if len(sys.argv) != 2:
+        print("Usage: python3 fly_in.py <maps/map_file>")
+        sys.exit(1)
+
+    map_file = sys.argv[1]
+
+    loader = Loader()
+    try:
+        raw_text = loader.load_file(map_file)
+        raw_dict = MapParser().parse(raw_text)
+        validated_param = MapConfigModel(**raw_dict)
+    except (FileNotFoundError, IsADirectoryError) as e:
+        print(f"\033[91m[FILE ERROR]\033[0m {e}")
+        sys.exit(1)
+    except PermissionError as e:
+        print(f"\033[91m[PERMISSION ERROR]\033[0m {e}")
+        sys.exit(1)
+    except ValueError as e:
+        print(f"\033[91m[VALUE ERROR]\033[0m {e}")
+        sys.exit(1)
+    except ValidationError as e:
+        print(f"\033[91m[INVALID MAP ERROR]\033[0m {e}")
+        sys.exit(1)
+    except Exception as e:
+        print(f"\033[91m[ERROR]\033[0m {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ loader.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+
+class Loader:
+    def load_file(self, file_path: str) -> str:
+        """
+        Loads a configuration file.
+        Handles cases: missing file, empty file, permission error
+        """
+        path = Path(file_path)
+        if not path.exists():
+            raise FileNotFoundError(f"Missing file: '{path.name}' not found")
+
+        try:
+            with open(path, mode="r", encoding="utf-8") as f:
+                content = f.read().strip()
+        except PermissionError:
+            raise PermissionError(
+                f"You don't have permission for the directory '{path.parent}'"
+                f"or for the file'{path.name}'"
+            )
+        except IsADirectoryError:
+            raise IsADirectoryError(
+                f"Expected a file, but '{path.name}' is a directory."
+            )
+
+        if not content:
+            raise ValueError(f"file '{path.name}' is empty.")
+
+        return content
